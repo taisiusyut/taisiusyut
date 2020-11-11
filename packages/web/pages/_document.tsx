c1bb2c17d369f354d95e474e7948840be724fea6
Pong <samfunghp@gmail.com>
2020-11-11 22:52:44 +0800

    feat(web): make it better

---
+++ packages/web/pages/_document.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Document, {
+  DocumentContext,
+  Html,
+  Head,
+  Main,
+  NextScript
+} from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return initialProps;
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+          <link rel="preload" href="/preload.js" as="script" />
+        </Head>
+        <body>
+          <script src="/preload.js" />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
