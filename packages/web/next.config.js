2b0600e6fa27b1a1ae8ef8b61465b4c07eb3bf87
Pong <samfunghp@gmail.com>
2020-12-15 22:49:33 +0800

    feat(web): add analyze

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,29 +1,39 @@
+const withPlugins = require('next-compose-plugins');
 const withPWA = require('next-pwa');
-
-module.exports = withPWA({
-  // Do not enable strict mode globally.
-  // Since @blueprintjs is violate the strict mode ...
-  reactStrictMode: false,
-  images: {
-    domains: ['res.cloudinary.com']
-  },
-  pwa: {
-    dest: 'public',
-    disable: process.env.NODE_ENV !== 'production'
-  },
-  exportPathMap: function () {
-    return {
-      '/explore': { page: '/' }
-    };
-  },
-  async rewrites() {
-    return process.env.NODE_ENV === 'production'
-      ? []
-      : [
-          {
-            source: '/api/:path*',
-            destination: 'http://localhost:5000/api/:path*'
-          }
-        ];
-  }
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
 });
+
+module.exports = withPlugins(
+  [
+    //
+    [withBundleAnalyzer]
+  ],
+  withPWA({
+    // Do not enable strict mode globally.
+    // Since @blueprintjs is violate the strict mode ...
+    reactStrictMode: false,
+    images: {
+      domains: ['res.cloudinary.com']
+    },
+    pwa: {
+      dest: 'public',
+      disable: process.env.NODE_ENV !== 'production'
+    },
+    exportPathMap: function () {
+      return {
+        '/explore': { page: '/' }
+      };
+    },
+    async rewrites() {
+      return process.env.NODE_ENV === 'production'
+        ? []
+        : [
+            {
+              source: '/api/:path*',
+              destination: 'http://localhost:5000/api/:path*'
+            }
+          ];
+    }
+  })
+);
