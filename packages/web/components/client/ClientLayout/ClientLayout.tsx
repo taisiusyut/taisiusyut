189439ff88a282720772150800b3ebbd98263950
Pong <samfunghp@gmail.com>
2020-12-22 23:16:05 +0800

    fix(client): single page

--- packages/web/components/client/ClientLayout/ClientLayout.tsx
+++ packages/web/components/client/ClientLayout/ClientLayout.tsx
@@ -1,13 +1,18 @@
 import { ReactNode, useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
+import { fromEvent } from 'rxjs';
+import { startWith } from 'rxjs/operators';
+import { useGoBack } from '@/hooks/useGoBack';
 import { BookShelfProvider } from '@/hooks/useBookShelf';
 import { BreakPointsProvider } from '@/hooks/useBreakPoints';
-import { ClientPreferencesProvider } from '@/hooks/useClientPreferences';
+import {
+  ClientPreferencesProvider,
+  useClientPreferencesState
+} from '@/hooks/useClientPreferences';
 import { BookShelf } from '../BookShelf';
 import { ClientSearch } from '../ClientSearch';
 import { BottomNavigation } from '../BottomNavigation';
 import classes from './ClientLayout.module.scss';
-import { useGoBack } from '@/hooks/useGoBack';
 
 interface Props {
   children?: ReactNode;
@@ -15,12 +20,28 @@ interface Props {
 
 const scrollRestoration: Record<string, number> = {};
 
+const breakPoint = 768;
+
 function ClientLayoutContent({ children }: Props) {
   const { asPath, events } = useRouter();
-  const [isSearch, setIsSearch] = useState(asPath.startsWith('/search'));
+  const { pagingDisplay } = useClientPreferencesState();
+  const [isSearching, setIsSearching] = useState(asPath.startsWith('/search'));
+  const [singlePage, setSinglePage] = useState(false);
+
   const isHome = /^\/(\?.*)?$/.test(asPath);
   const isFeatured = /^\/featured(\?.*)?$/.test(asPath);
+  const isSearch = asPath.startsWith('/search');
   const goback = useGoBack();
+
+  useEffect(() => {
+    const subscription = fromEvent(window, 'resize')
+      .pipe(startWith(null))
+      .subscribe(() => {
+        setSinglePage(!pagingDisplay || window.innerWidth <= breakPoint);
+      });
+
+    return () => subscription.unsubscribe();
+  }, [pagingDisplay]);
 
   // scroll restoration for mobile view
   useEffect(() => {
@@ -48,19 +69,17 @@ function ClientLayoutContent({ children }: Props) {
 
   useEffect(
     () =>
-      setIsSearch(isSearch =>
-        window.innerWidth > 768
-          ? isSearch || asPath.startsWith('/search')
-          : asPath.startsWith('/search')
+      setIsSearching(isSearching =>
+        window.innerWidth > breakPoint ? isSearching || isSearch : isSearch
       ),
-    [asPath]
+    [asPath, isSearch]
   );
 
   return (
     <div
       className={[
         classes['layout'],
-        isHome || isSearch ? classes['show-left-panel'] : ''
+        isHome || isSearching ? classes['show-left-panel'] : ''
       ]
         .join(' ')
         .trim()}
@@ -68,22 +87,30 @@ function ClientLayoutContent({ children }: Props) {
       <div className={classes['layout-content']}>
         {/* Should not unmount the left panel component. 
             Because the first-time rendering and scroll position restoration not smooth */}
-        <div className={classes['left-panel']} hidden={isSearch}>
+        <div
+          className={classes['left-panel']}
+          hidden={(singlePage && !isHome) || isSearching}
+        >
           <BookShelf />
         </div>
-        <div className={classes['left-panel']} hidden={!isSearch}>
+        <div
+          className={classes['left-panel']}
+          hidden={(singlePage && !isSearch) || !isSearching}
+        >
           <ClientSearch
             onLeave={() =>
               goback({ targetPath: ['/', '/featured'] }).then(() =>
-                setIsSearch(false)
+                setIsSearching(false)
               )
             }
           />
         </div>
-        <div className={classes['right-panel']}>{children}</div>
+        {(!singlePage || (!isHome && !isSearch)) && (
+          <div className={classes['right-panel']}>{children}</div>
+        )}
       </div>
       <div className={classes['bottom-navigation']}>
-        {(isHome || isFeatured || isSearch) && <BottomNavigation />}
+        {(isHome || isFeatured || isSearching) && <BottomNavigation />}
       </div>
     </div>
   );
