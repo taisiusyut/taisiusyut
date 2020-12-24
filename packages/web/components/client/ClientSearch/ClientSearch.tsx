0c48d3300ab3045155903133683dcee05755c64d
Pong <samfunghp@gmail.com>
2020-12-24 12:12:29 +0800

    fix(client): code review

--- packages/web/components/client/ClientSearch/ClientSearch.tsx
+++ packages/web/components/client/ClientSearch/ClientSearch.tsx
@@ -1,22 +1,15 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-import { AxiosError } from 'axios';
-import { EMPTY, fromEvent, merge } from 'rxjs';
-import {
-  catchError,
-  distinctUntilChanged,
-  mergeMap,
-  startWith,
-  map,
-  tap
-} from 'rxjs/operators';
 import { ClientHeader } from '@/components/client/ClientLayout';
 import { ButtonPopover } from '@/components/ButtonPopover';
-import { createUseCRUDReducer } from '@/hooks/crud-reducer';
-import { getBooks } from '@/service';
-import { Toaster } from '@/utils/toaster';
-import { useForm, transoform, ClientSearchInput } from './ClientSearchInput';
-import { ClientSearchItem, Book } from './ClientSearchItem';
+import { useSearchResult } from './useSearchResult';
+import {
+  useForm,
+  transoform,
+  ClientSearchInput,
+  Store
+} from './ClientSearchInput';
+import { ClientSearchItem } from './ClientSearchItem';
 import { ClientSearchNotFound } from './ClientSearchNotFound';
 import classes from './ClientSearch.module.scss';
 
@@ -24,26 +17,47 @@ interface Props {
   onLeave: () => void;
 }
 
-const useBookReducer = createUseCRUDReducer<Book, 'id'>('id', {
-  initializer: state => ({ ...state, pageSize })
-});
-
-const pageSize = 10;
-
-const placeholder = Array.from<void, Book>({ length: pageSize }, (_, idx) => ({
-  id: String(idx)
-}));
-
 const createId = (idx: number) => `search-result-${idx}`;
 
 export function ClientSearch({ onLeave }: Props) {
-  const { asPath, query, push } = useRouter();
+  const router = useRouter();
+  const { asPath, query } = router;
+  const [form] = useForm();
   const [search, setSearch] = useState(() =>
     transoform(asPath.startsWith('/search') ? query : {})
   );
-  const [state, actions] = useBookReducer();
-  const [form] = useForm();
-  const scrollerRef = useRef<HTMLDivElement>(null);
+  const { state, scrollerRef } = useSearchResult(search);
+
+  const handleSearch = (search: Store) => {
+    setSearch(search);
+    router.push(
+      {
+        pathname: '/search',
+        query: search.value ? { [search.type]: search.value } : undefined
+      },
+      undefined,
+      { shallow: true }
+    );
+  };
+
+  const searchButton = (
+    <ButtonPopover minimal icon="cross" content="取消搜索" onClick={onLeave} />
+  );
+
+  const items = state.list.map((book, idx) => (
+    <ClientSearchItem key={book.id} book={book} className={createId(idx)} />
+  ));
+
+  const notFound = state.list.length === 0 && search.value?.trim() && (
+    <ClientSearchNotFound
+      className={classes['not-found']}
+      searchType={search.type}
+    />
+  );
+
+  useEffect(() => {
+    form.setFieldsValue(search);
+  }, [search, form]);
 
   useEffect(() => {
     if (asPath.startsWith('/search')) {
@@ -56,125 +70,19 @@ export function ClientSearch({ onLeave }: Props) {
     }
   }, [asPath, query]);
 
-  useEffect(() => {
-    form.setFieldsValue(search);
-  }, [search, form]);
-
-  useEffect(() => {
-    const scroller = scrollerRef.current;
-
-    if (!scroller) {
-      throw new Error(`scroller is not defined`);
-    }
-
-    const scroller$ = fromEvent(scroller, 'scroll').pipe(
-      map(
-        () =>
-          [
-            scroller.scrollTop,
-            scroller.offsetHeight + scroller.offsetTop
-          ] as const
-      )
-    );
-
-    const windowScroll$ = fromEvent(window, 'scroll').pipe(
-      map(() => [window.scrollY, window.innerHeight] as const)
-    );
-
-    let pageNo = 1;
-
-    if (search.type && search.value?.trim()) {
-      const subscription = merge(scroller$, windowScroll$)
-        .pipe(
-          startWith([0, 0]),
-          map(([scrollTop, offsetHeight]) => {
-            const nextIndex = pageNo * pageSize;
-            const target = document.querySelector<HTMLElement>(
-              `.${createId(nextIndex)}`
-            );
-            if (target && scrollTop + offsetHeight >= target.offsetTop) {
-              pageNo += 1;
-            }
-            return pageNo;
-          }),
-          distinctUntilChanged(),
-          tap(pageNo => {
-            if (pageNo === 1) {
-              // reset scroll position
-              window.scrollTo(0, 0);
-              scroller.scrollTop = 0;
-              actions.list(placeholder);
-            }
-          }),
-          mergeMap(pageNo => getBooks({ [search.type]: search.value, pageNo })),
-          catchError((error: AxiosError) => {
-            if (error.config.params?.pageNo === 1) {
-              actions.list([]);
-            }
-            Toaster.apiError(`Get books failure`, error);
-            return EMPTY;
-          })
-        )
-        .subscribe(payload => {
-          actions.paginate(payload);
-        });
-      return () => subscription.unsubscribe();
-    } else {
-      // for clear search result
-      actions.list([]);
-    }
-  }, [actions, search]);
-
   return (
     <div className={classes['search']}>
-      <ClientHeader
-        title="搜索書籍"
-        left={
-          <ButtonPopover
-            minimal
-            icon="cross"
-            content="取消搜索"
-            onClick={onLeave}
-          />
-        }
-      />
+      <ClientHeader title="搜索書籍" left={searchButton} />
+
       <div className={classes['content']}>
-        <ClientSearchInput
-          form={form}
-          onFinish={search => {
-            setSearch(search);
-            push(
-              {
-                pathname: '/search',
-                query: search.value
-                  ? { [search.type]: search.value }
-                  : undefined
-              },
-              undefined,
-              { shallow: true }
-            );
-          }}
-        />
+        <ClientSearchInput form={form} onFinish={handleSearch} />
+
         <div className={classes['books']} ref={scrollerRef}>
-          {
-            <>
-              <div className={classes['border']} />
-              {state.list.map((book, idx) => (
-                <ClientSearchItem
-                  key={book.id}
-                  book={book}
-                  className={createId(idx)}
-                />
-              ))}
-            </>
-          }
+          <div className={classes['border']} />
+          {items}
         </div>
-        {state.list.length === 0 && search.value?.trim() && (
-          <ClientSearchNotFound
-            className={classes['not-found']}
-            searchType={search.type}
-          />
-        )}
+
+        {notFound}
       </div>
     </div>
   );
---
+++ packages/web/components/client/ClientSearch/useSearchResult.ts
@@ -0,0 +1,100 @@
+import { useEffect, useRef } from 'react';
+import { AxiosError } from 'axios';
+import { EMPTY, fromEvent, merge } from 'rxjs';
+import {
+  catchError,
+  distinctUntilChanged,
+  mergeMap,
+  startWith,
+  map,
+  tap
+} from 'rxjs/operators';
+import { createUseCRUDReducer } from '@/hooks/crud-reducer';
+import { getBooks } from '@/service';
+import { Toaster } from '@/utils/toaster';
+import { Store } from './ClientSearchInput';
+import { Book } from './ClientSearchItem';
+
+const pageSize = 10;
+
+const placeholder = Array.from<void, Book>({ length: pageSize }, (_, idx) => ({
+  id: String(idx)
+}));
+
+const useBookReducer = createUseCRUDReducer<Book, 'id'>('id', {
+  initializer: state => ({ ...state, pageSize })
+});
+
+export const createId = (idx: number) => `search-result-${idx}`;
+
+export function useSearchResult(search: Store) {
+  const [state, actions] = useBookReducer();
+  const scrollerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const scroller = scrollerRef.current;
+
+    if (!scroller) {
+      throw new Error(`scroller is not defined`);
+    }
+
+    const scroller$ = fromEvent(scroller, 'scroll').pipe(
+      map(
+        () =>
+          [
+            scroller.scrollTop,
+            scroller.offsetHeight + scroller.offsetTop
+          ] as const
+      )
+    );
+
+    const windowScroll$ = fromEvent(window, 'scroll').pipe(
+      map(() => [window.scrollY, window.innerHeight] as const)
+    );
+
+    let pageNo = 1;
+
+    if (search.type && search.value?.trim()) {
+      const subscription = merge(scroller$, windowScroll$)
+        .pipe(
+          startWith([0, 0]),
+          map(([scrollTop, offsetHeight]) => {
+            const nextIndex = pageNo * pageSize;
+            const target = document.querySelector<HTMLElement>(
+              `.${createId(nextIndex)}`
+            );
+            if (target && scrollTop + offsetHeight >= target.offsetTop) {
+              pageNo += 1;
+            }
+            return pageNo;
+          }),
+          distinctUntilChanged(),
+          tap(pageNo => {
+            if (pageNo === 1) {
+              // reset scroll position
+              window.scrollTo(0, 0);
+              scroller.scrollTop = 0;
+              actions.list(placeholder);
+            }
+          }),
+          mergeMap(pageNo => getBooks({ [search.type]: search.value, pageNo })),
+          catchError((error: AxiosError) => {
+            if (error.config.params?.pageNo === 1) {
+              actions.list([]);
+            }
+            Toaster.apiError(`Get books failure`, error);
+            return EMPTY;
+          })
+        )
+        .subscribe(payload => {
+          actions.paginate(payload);
+        });
+      return () => subscription.unsubscribe();
+    } else {
+      // for clear search result
+      actions.list([]);
+    }
+  }, [actions, search]);
+
+  return { state, scrollerRef };
+}
