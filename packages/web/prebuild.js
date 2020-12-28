aae6216020074b79a75a343bb9237d483af37d84
Pong <samfunghp@gmail.com>
2020-12-28 20:05:36 +0800

    fix(client): initial build

--- packages/web/prebuild.js
+++ packages/web/prebuild.js
@@ -1,5 +1,14 @@
 // @ts-check
 
+const fs = require('fs');
+const path = require('path');
+
+const distDir = path.resolve(process.cwd(), `./.next`);
+
+if (!fs.existsSync(distDir)) {
+  fs.mkdirSync(distDir);
+}
+
 const {
   clearFeaturedData,
   getClientFeaturedPageData
