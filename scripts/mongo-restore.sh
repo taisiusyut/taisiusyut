3a33c7650542bce670bd42ca0663cdf74304059b
Pong <samfunghp@gmail.com>
2021-03-16 14:01:29 +0800

    fix(): mongo sh

--- scripts/mongo-restore.sh
+++ scripts/mongo-restore.sh
@@ -3,3 +3,5 @@
 # sh scripts/mongo-restore.sh <dump_file_name>
 
 docker exec -i mongo sh -c 'mongorestore -d taisiusyut --archive' < $1.dump
+
+# docker exec -i mongo sh -c 'mongorestore --nsFrom "DB_NAME.*" --nsTo "taisiusyut.*" --archive' < $1.dump
---
+++ scripts/mongo-start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run -it -p 27017:27017 --name mongo -d mongo
