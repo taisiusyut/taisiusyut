f74f325481c37a1bdf9669cdfa38821f193dac22
Pong <samfunghp@gmail.com>
2021-03-25 21:36:23 +0800

    fix(): typo

--- packages/web/pages/_document.tsx
+++ packages/web/pages/_document.tsx
@@ -35,7 +35,6 @@ class MyDocument extends Document {
               __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`
             }}
           />
-          <script src="/preload.js" />
           <Main />
           <NextScript />
           <InlineScript
