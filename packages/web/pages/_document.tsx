7ff5256513fa0f725bced7258731592091f41521
Pong <samfunghp@gmail.com>
2021-03-26 12:12:43 +0800

    fix(): loading

--- packages/web/pages/_document.tsx
+++ packages/web/pages/_document.tsx
@@ -16,6 +16,8 @@ class MyDocument extends Document {
       >
         <Head>
           <InlineScript fn={preload} />
+          <link rel="preconnect" href="https://www.google-analytics.com" />
+          <link rel="dns-prefetch" href="https://www.google-analytics.com" />
         </Head>
         <Meta />
         <body>
@@ -26,6 +28,15 @@ class MyDocument extends Document {
           />
           <Main />
           <NextScript />
+          <InlineScript
+            fn={`
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer', '${process.env.NEXT_PUBLIC_GTM_ID}');
+            `}
+          />
         </body>
       </Html>
     );
