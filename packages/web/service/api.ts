534ef53195bf91ab46f98e0aaf331cf019045951
Pong <samfunghp@gmail.com>
2021-03-27 23:36:37 +0800

    feat(client): add headless checking for api

--- packages/web/service/api.ts
+++ packages/web/service/api.ts
@@ -1,5 +1,8 @@
 import axios, { AxiosRequestConfig, CustomAxiosInstance } from 'axios';
 import { ValidationHeader } from '@/constants';
+import { isHeadless } from '@/utils/isHeadless';
+
+let headLess: boolean | null = null;
 
 function createCustomAxiosInstance(
   config: AxiosRequestConfig
@@ -7,12 +10,21 @@ function createCustomAxiosInstance(
   const instance = axios.create(config);
 
   instance.interceptors.request.use(config => {
+    if (headLess === null) {
+      headLess = !!isHeadless();
+    }
+    if (headLess === true) {
+      throw new Error(`headless browser`);
+    }
+    return config;
+  });
+
+  instance.interceptors.request.use(config => {
     config.headers = {
       ...config.headers,
       [ValidationHeader]:
         process.env.NEXT_PUBLIC_VALIDATION_HEADER || ValidationHeader
     };
-
     return config;
   });
 
