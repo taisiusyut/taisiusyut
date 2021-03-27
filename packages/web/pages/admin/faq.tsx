b7e664b9cc282f5a037357fbcd7786eab1df5c47
Pong <samfunghp@gmail.com>
2021-03-28 00:15:33 +0800

    fix(): build

--- packages/web/pages/admin/faq.tsx
+++ packages/web/pages/admin/faq.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { AdminLayout } from '@/components/admin/AdminLayout';
 import { AdminFAQ } from '@/components/admin/AdminFAQ';
+import { UserRole } from '@/typings';
 
 export default function FAQPage() {
   return <AdminFAQ />;
 }
 
+FAQPage.access = [UserRole.Root, UserRole.Admin, UserRole.Author];
 FAQPage.layout = AdminLayout;
