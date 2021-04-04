a78fe0614600715ebac30ec5c9db91c37bb23e9f
Pong <samfunghp@gmail.com>
2021-04-04 15:22:17 +0800

    feat(app): add ts config paths

--- packages/app/babel.config.js
+++ packages/app/babel.config.js
@@ -1,6 +1,45 @@
-module.exports = function(api) {
+// @ts-check
+const fs = require('fs');
+const path = require('path');
+
+/**
+ * @param {*} api
+ */
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            /**
+             * @param {string[]} param0
+             * @returns {string}
+             */
+            '@/utils': ([pathname]) => {
+              const commonFilePath = path.join(
+                __dirname,
+                '../../node_modules/@taisiusyut/common',
+                pathname.slice(1)
+              );
+              const filePath = path.join(__dirname, pathname.slice(1));
+
+              for (const p of [commonFilePath, filePath]) {
+                const resolved = require.resolve(p);
+                if (fs.existsSync(resolved)) {
+                  console.log('module-resolver', resolved);
+                  return resolved;
+                }
+              }
+              return pathname;
+            },
+            '@': __dirname
+          },
+          extensions: ['.js', '.jsx', '.es', '.es6', '.mjs', '.ts', '.tsx']
+        }
+      ]
+    ]
   };
 };
--- packages/app/metro.config.js
+++ packages/app/metro.config.js
@@ -1,4 +1,3 @@
-// @ts-check
 const { createMetroConfiguration } = require('expo-yarn-workspaces');
 
 module.exports = createMetroConfiguration(__dirname);
